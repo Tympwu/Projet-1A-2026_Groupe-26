8151a41a4264ea0a3b4d9b1d83a9288f37e1b155
Simon-LanTino <simon.langlois-tino@eleve.ensai.fr>
2026-03-11 11:43:41 +0000

    class player

--- src/Model/Player.py
+++ src/Model/Player.py
@@ -1,15 +1,27 @@
-from typing import Union
-from src.Common.utils import parse_boolean
+# from typing import Union
+# from src.Common.utils import parse_boolean
+
+# is_the_goat: Union[str, bool]
 
 
 class Player:
-    def __init__(self, id: int, full_name: str, is_the_goat: Union[str, bool]):
+    def __init__(self, id: int, full_name: str, sport: str, nb_jours_blesse: int, age: int, palmares: int = 0):
         self.id = id
         self.full_name = full_name
-        self.is_the_goat = parse_boolean(is_the_goat)
-
+        # self.is_the_goat = parse_boolean(is_the_goat)
+        self.palmares = palmares
+        self.nb_jours_blesse = nb_jours_blesse
+        self.age = age
+        
     def __repr__(self):
-        display_string = self.full_name
-        if self.is_the_goat:
-            display_string += " (GOAT)"
-        return display_string
+        # display_string = self.full_name
+        # if self.is_the_goat:
+        # display_string += " (GOAT)"
+        # return display_string
+        return f"Nom:{self.full_name}, Id:{self.id}, Sport:{self.sport}"
+
+    def __str__(self):
+        return f"Le joueur {self.id} s'appelle {self.full_name} et se distingue en {self.sport}."
+
+    def victoire_finale(self):
+        self.palmares += 1
