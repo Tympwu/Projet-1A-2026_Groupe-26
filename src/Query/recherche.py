b46f32a81e45afa2cd168bf58c63b0b104657bf2
AlexYu3023 <alexandre.yu@eleve.ensai.fr>
2026-04-02 10:00:34 +0000

    modif recherche

--- src/Query/recherche.py
+++ src/Query/recherche.py
@@ -5,9 +5,39 @@ from ..DAO.interaction import DAO
 class Recherche:
     def __init__(self, sport: str):
         self.sport = sport
-        self.dao = None
+        self.dao = {}
+
+    def data_basketball():
+        self.dao["game"]=DAO(fichier = "../data/basketball/player.csv")
+        self.dao["player"]=DAO(fichier = )
+        self.dao["team"]=DAO(fichier = )
+
+    def data_football_european_leagues():
+        self.dao[]
+
+    def data_league_of_legends():
+
+    def data_tennis():
+
+    def data_volleyball():
+
+    def handle_unknown():
+        log.write("unknown role")
+        raise PermissionError("Unknown role")
 
     def cherche(self):
         if self.sport == "basketball":
 
+        if self.sport == "football_european_leagues":
+
+        if self.sport == "league_of_legends":
+
         if self.sport == "tennis":
+
+        if self.sport == "volleyball":
+    
+    action = {
+
+    }
+
+    actions.get(status, handle_unknown)()
