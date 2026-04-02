626f936bdda7082fec6774ff1f7a5bdc6b31e2fc
AlexYu3023 <alexandre.yu@eleve.ensai.fr>
2026-04-02 10:19:55 +0000

    modif recherche et interaction

--- src/Query/recherche.py
+++ src/Query/recherche.py
@@ -8,36 +8,48 @@ class Recherche:
         self.dao = {}
 
     def data_basketball():
-        self.dao["game"]=DAO(fichier = "../data/basketball/player.csv")
-        self.dao["player"]=DAO(fichier = )
-        self.dao["team"]=DAO(fichier = )
+        self.dao["game"] = DAO(fichier = "data/basketball/game.csv")
+        self.dao["player"] = DAO(fichier = "data/basketball/player.csv")
+        self.dao["team"] = DAO(fichier = "data/basketball/team.csv" )
 
     def data_football_european_leagues():
-        self.dao[]
+        self.dao["country"] = DAO(fichier = "data/football_european_leagues/country.csv")
+        self.dao["league"] = DAO(fichier = "data/football_european_leagues/league.csv")
+        self.dao["match"] = DAO(fichier = "data/football_european_leagues/")
+        self.dao["player"] = DAO(fichier = "data/football_european_leagues/")
+        self.dao["team"] = DAO(fichier = "data/football_european_leagues/")
+        
 
     def data_league_of_legends():
+        self.dao["coach"] = DAO(fichier = "data/league_of_legends/coach.csv")
+        self.dao["match"] = DAO(fichier = "data/league_of_legends/match.csv")
+        self.dao["player"] = DAO(fichier = "data/league_of_legends/player.csv")
+        self.dao["team"] = DAO(fichier = "data/league_of_legends/team.csv")
 
     def data_tennis():
-
+        self.dao[""] = DAO(fichier = )
+        self.dao[""] = DAO(fichier = )
+        self.dao[""] = DAO(fichier = )
+        self.dao[""] = DAO(fichier = )
+    
     def data_volleyball():
+        self.dao[""] = DAO(fichier = )
+        self.dao[""] = DAO(fichier = )
+        self.dao[""] = DAO(fichier = )
+        self.dao[""] = DAO(fichier = )
+        self.dao[""] = DAO(fichier = )
+        self.dao[""] = DAO(fichier = )
+        self.dao[""] = DAO(fichier = )
 
     def handle_unknown():
-        log.write("unknown role")
-        raise PermissionError("Unknown role")
+        raise ValueError("Sport non pris en charge")
 
     def cherche(self):
-        if self.sport == "basketball":
-
-        if self.sport == "football_european_leagues":
-
-        if self.sport == "league_of_legends":
-
-        if self.sport == "tennis":
-
-        if self.sport == "volleyball":
-    
-    action = {
-
-    }
-
-    actions.get(status, handle_unknown)()
+        action = {
+            "basketball": data_basketball
+            "football_european_leagues": data_football_european_leagues
+            "league_of_legends": data_league_of_legends
+            "tennis": data_tennis
+            "volleyball": data_volleyball
+        }
+        actions.get(status, handle_unknown)()
