58087c57e21aecaca9d024fd471f408b473e621d
Simon-LanTino <simon.langlois-tino@eleve.ensai.fr>
2026-04-02 10:46:43 +0000

    player et personne bien avancés

--- src/Model/Personne.py
+++ src/Model/Personne.py
@@ -3,15 +3,19 @@ from typing import Any
 
 class Personne:
 
-    def __init__(self, first_name: str, last_name: str,  age: int, sexe: str = None) -> None:
-        if not isinstance(first_name, str):
-            raise TypeError("'first_name' doit être une instance de str")
-        if not isinstance(sexe, str) and sexe in ("femme", "homme", None):
-            raise TypeError("'sexe' doit être une instance de str dans 'femme', 'homme' ou None")
-        if not isinstance(age, int):
-            raise TypeError("'age' doit être une instance de int")
+    def __init__(
+        self,
+        first_name: str,
+        last_name: str,
+        full_name: str,
+        age: int,
+        lieu_naissance: str,
+        sexe: str = None
+    ) -> None:
         self.first_name = first_name
         self.last_name = last_name
+        if full_name is None:
+            self.full_name = self.first_name + self.last_name
         self.age = age
         self.sexe = sexe
 
@@ -21,6 +25,7 @@ class Personne:
         return NotImplemented
 
     def __str__(self) -> str:
-        return f"""Nom: {self.first_name} {self.last_name}\n
+        return f"""Nom: {self.full_name}\n
         Age: {self.age}\n
-        Sexe: {self.sexe}."""
+        Sexe: {self.sexe}\n
+        Lieu de naissance: {self.lieu_naissance}."""
--- src/Model/Player.py
+++ src/Model/Player.py
@@ -1,5 +1,6 @@
 
 from .Personne import Personne
+from typing import Any
 
 
 class Player(Personne):
@@ -9,34 +10,30 @@ class Player(Personne):
         first_name: str = None,
         last_name: str = None,
         full_name: str = None,
-        pseudo: str = None,
-        equipe: str = None,
-        nationalite_equipe: str = None,
-        lieu_naissance: str = None,
-        sport: str = None,
+        sexe: str = None,
         age: int = None,
+        lieu_naissance: str = None,
+        pseudo: str = None,
+        equipe: Any = None,
+        nationalite_equipe: str = None,
+        sport: str = None,
         poste: str = None,
         numero_maillot: int = None,
         main_forte: str = None,
         taille: int = None,
-        poids: int = None,
-        blessé: bool = False,
-        sexe: str = None,
-        palmares: list[str] = None
+        poids: int = None
     ) -> None:
-        super().__init__(first_name, last_name, age, sexe)
-        if not isinstance(id, int):
-            raise TypeError("'id' doit être une instance de int")
-        if not isinstance(sport, str):
-            raise TypeError("'sport' doit être une instance de str")
-        if not isinstance(blessé, str) and blessé in ("blesse", "en_forme", None):
-            raise TypeError("'etat_physique' doit être une instance de etat_physique dans ('blesse', 'en_forme', None)")
-        if not all(isinstance(p, str) for p in palmares):
-            raise TypeError("palmares doit être une liste de str")
+        super().__init__(first_name, last_name, full_name, lieu_naissance, age, sexe)
         self.id = id
+        self.pseudo = pseudo
+        self.equipe = equipe
+        self.nationalite_equipe = nationalite_equipe
         self.sport = sport
-        self.palmares = palmares
-        self.blessé = blessé
+        self.poste = poste
+        self.numero_maillot = numero_maillot
+        self.main_forte = main_forte
+        self.taille = taille
+        self.poids = poids
     
     def __repr__(self) -> str:
         return f"Player( Nom:{self.full_name}, Sport:{self.sport})"
@@ -46,13 +43,5 @@ class Player(Personne):
         ID: {self.id}\n
         Sport: {self.sport}\n
         Age: {self.age} ans\n
-        Palmarès: {self.palmares}\n
-        Blessé{"e" if self.sexe == "femme" else ""}: {"Oui" if self.blessé is True else "Non"}."""
+        """
 
-    def blessure(self) -> None:
-        if self.blessé is False or self.blessé is None:
-            self.blessé = True
-
-    def retabli(self) -> None:
-        if self.blessé is True or self.blessé is None:
-            self.blessé = False
