c70bd462dec6e77f3e25345776ff1d387b0068ed
AlexYu <alexandre.yu@eleve.ensai.fr>
2026-04-07 12:21:10 +0200

    modif ajout parser_badminton

--- src/Query/Parser.py
+++ src/Query/Parser.py
@@ -59,9 +59,6 @@ class Tennis_Parser(Parser):
             self.list_player[player.id] = player
 
     def parse_competition(self, data: pd.DataFrame):
-
-
-
         pass
 
     def parse_matches(self, data: pd.DataFrame):
@@ -71,7 +68,44 @@ class Tennis_Parser(Parser):
         pass
 
 
+class Badminton_Parser(Parser):
+    def __init__(self):
+        super().__init__("badminton")
+    
+    def parse_players(self, data: pd.DataFrame):
+        self.list_player = {}
+        # --- Fetch full name and continent ---
+        full_name = self.fetch_safety_data(row["name_full"], str).strip()
+        continent = self.fetch_safety_data(row["continent"], str).strip()
 
+        # --- Split the name into parts ---
+        parts = full_name.split()
+
+        # --- Determine first_name and last_name according to continent ---
+        if continent.lower() == "africa":
+            first_name = parts[0]
+            last_name = " ".join(parts[1:])  # everything else
+        else:
+            first_name = " ".join(parts[:-1])  # all but last
+            last_name = parts[-1]              # last word
+        for index, row in data.iterrows():
+            player = Player(
+                id=index,
+                first_name=first_name,
+                last_name=last_name,
+                nationalite=self.fetch_safety_data(row["country"], str),
+                continent = self.fetch_safety_data(row["continent"], str),
+                sport="badminton")
+            self.list_player[player.id] = player
+
+    def parse_competition(self, data: pd.DataFrame):
+        pass
+
+    def parse_matches(self, data: pd.DataFrame):
+        pass
+
+
+    
 
 
 
@@ -83,9 +117,8 @@ class Basketball_Parser(Parser):
 
 
 class Football_Parser(Parser):
-    
-def __init__(self):
-        super().__init__("football")
+    def __init__(self):
+            super().__init__("football")
 
     def parse_players(self, data: pd.DataFrame):
         self.list_player = {}
