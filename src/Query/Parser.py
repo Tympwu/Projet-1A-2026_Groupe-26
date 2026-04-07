ee535df4bf86d86099a9ae0063d780740c9d0e3b
jeanpohardy <jean.pohardy@eleve.ensai.fr>
2026-04-07 09:49:03 +0000

    1ere modif parser foot

--- src/Query/Parser.py
+++ src/Query/Parser.py
@@ -72,3 +72,32 @@ class Tennis_Parser(Parser):
 
 class Football_Parser(Parser):
     
+def __init__(self):
+        super().__init__("football")
+
+    def parse_players(self, data: pd.DataFrame):
+        self.list_player = {}
+        for index, row in data.iterrows():
+            dob = str(self.fetch_safety_data(row["dob"], int))
+            dob = dob[:4] + "-" + dob[4:6] + "-" + dob[6:8]
+            print(dob)
+            player = Player(
+                id=self.fetch_safety_data(row["player_id"], int),
+                sexe="H",
+                first_name=self.fetch_safety_data(row["name_first"], str),
+                last_name=self.fetch_safety_data(row["name_last"], str),
+                main_forte=self.fetch_safety_data(row["hand"], str),
+                dob=dob,
+                nationalite=self.fetch_safety_data(row["ioc"], str),
+                taille=self.fetch_safety_data(row["height"], int),
+                sport="Tennis")
+            self.list_player[player.id] = player
+
+    def parse_competition(self, data: pd.DataFrame):
+        pass
+
+    def parse_matches(self, data: pd.DataFrame):
+        pass
+
+    def parse_team(self, data: pd.DataFrame):
+        pass
