346fabff169e4437501714b57c593e49564ed862
Simon-LanTino <simon.langlois-tino@eleve.ensai.fr>
2026-04-07 10:49:55 +0000

    basket et volley

--- src/Query/Parser.py
+++ src/Query/Parser.py
@@ -76,11 +76,53 @@ class Tennis_Parser(Parser):
 
 
 class Volleyball_Parser(Parser):
+    def __init__(self):
+        super().__init__("volleyball")
 
+    def parse_players(self, data: pd.DataFrame):
+        self.list_player = {}
+        for index, row in data.iterrows():
+            player = Player(
+                sexe="H",
+                full_name=self.fetch_safety_data(row["name"], str),
+                pseudo=self.fetch_safety_data(row["nickname"], str),
+                lieu_naissance=self.fetch_safety_data(row["birth_place"], str),
+                dob=str(self.fetch_safety_data(row["birthdate"], int)),
+                nationalite=self.fetch_safety_data(row["country_code"], str),
+                taille=self.fetch_safety_data(row["height"], int),
+                sport="Volleyball")
+            self.list_player[player.id] = player
 class League_of_legend_Parser(Parser):
 
-class Basketball_Parser(Parser):
 
+class Basketball_Parser(Parser):
+    def __init__(self):
+        super().__init__("tennis")
+
+    def parse_players(self, data: pd.DataFrame, other=None):
+        for index, row in data.iterrows():
+            dob = str(self.fetch_safety_data(row["birthdate"], int))
+            if len(dob) != 10:
+                dob = dob[:9] + "0"
+                print(dob)
+            taille=self.fetch_safety_data(row["height"], str)
+            taille1=int(taille[0])
+            taille2=int(taille[2:])
+            taille=30,48*taille1 + 2,54*taille2
+            poids=self.fetch_safety_data(row["weight"], int)
+            poids=0,454*poids
+            player = Player(
+                id=self.fetch_safety_data(row["person_id"], int),
+                first_name=self.fetch_safety_data(row["first_name"], str),
+                last_name=self.fetch_safety_data(row["last_name"], str),
+                numero_maillot=self.fetch_safety_data(row["jersey"], int),
+                dob=dob,
+                role=self.fetch_safety_data(row["position"], str),
+                taille=taille,
+                poids=poids,
+                equipe=dict_equipe[self.fetch_safety_data(row["team_id"], int)],
+                sport="Basketball")
+            self.dict_player_player[player.id] = player
 
 class Football_Parser(Parser):
     
