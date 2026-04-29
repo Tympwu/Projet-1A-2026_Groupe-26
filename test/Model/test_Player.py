713e24f44594f4955705273dda84f3836b586a65
jeanpohardy <jean.pohardy@eleve.ensai.fr>
2026-04-29 10:48:08 +0000

    implémentation des tests de Personne (fin)

--- test/Model/test_Player.py
+++ test/Model/test_Player.py
@@ -1,3 +1,7 @@
+import re
+
+import pytest
+
 from src.Model.Player import Player
 
 """
@@ -15,3 +19,30 @@ def test_player_repr_method_displays_goat_tag_for_the_goat():
     assert str(the_goat) == "Arthur Friedenreich (GOAT)"
 """
 
+def test_Player_init():
+    P1 = Player(
+        id=1, first_name="Edouard", last_name="Elric", sexe="M",
+        dob="04-10-2003", lieu_naissance="Resembool", pseudo="ED",
+        id_equipe=1, nationalite="Amestris", continent="Fictif",
+        numero_maillot="66", main_forte="droite", taille=149.5,
+        poid=50, role="Alchemist"
+        )
+    P2 = Player()
+    assert P1.id == 1
+    assert P1.first_name == "Edouard"
+    assert P1.last_name == "Elric"
+    assert P1.full_name == "Edouard Elric"
+    assert P1.dob == "04-10-2003"
+    assert P1.lieu_naissance == "Resembool"
+    assert P2.id is None
+    assert P2.last_name is None
+    assert P2.first_name is None
+    assert P2.full_name is None
+    assert P2.lieu_naissance is None
+    assert P2.pseudo is None
+    assert P2.id_equipe is None
+    assert P2.equipe is None
+    assert P2.sexe is None
+    assert P2.sexe is None
+    assert P2.sexe is None
+    assert P2.sexe is None
